917f12687d04fd655670121ac7f5efe3ce1b0354
Kiaron <kiagov01@gmail.com>
2025-04-01 19:31:54 +0200

    restapi

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -7,32 +7,36 @@ def initiate():
         {"name":"Audi", "description":"Great cars. German technology"},
         {"name":"Kia", "description":"Great cars. Korean technology"},
         {"name":"Toyota", "description":"Great cars. Japanese technology"},
+        {"name":"fsfwefwer", "description":"Great cars. Japanese technology"},
     ]
 
-    car_make_instances = []
+    car_make_instances = {}
     for data in car_make_data:
-            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
-
+        car_make, _ = CarMake.objects.get_or_create(name=data['name'], defaults={"description": data['description']})
+        car_make_instances[data['name']] = car_make  # Store for reference
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
-      {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
-      {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
-      {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
-      {"name":"A4", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
-      {"name":"A5", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
-      {"name":"A6", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
-      {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":car_make_instances[3]},
-      {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":car_make_instances[3]},
-      {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":car_make_instances[3]},
-      {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4]},
-      {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4]},
-      {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":car_make_instances[4]},
-        # Add more CarModel instances as needed
+      {"name":"Pathfinder", "type":"SUV", "year": 2023, "dealer_id": 1, "car_make":"NISSAN"},
+      {"name":"Qashqai", "type":"SUV", "year": 2023, "dealer_id": 2, "car_make":"NISSAN"},
+      {"name":"XTRAIL", "type":"SUV", "year": 2023, "dealer_id": 3, "car_make":"NISSAN"},
+      {"name":"A-Class", "type":"SUV", "year": 2023, "dealer_id": 4, "car_make":"Mercedes"},
+      {"name":"C-Class", "type":"SUV", "year": 2023, "dealer_id": 5, "car_make":"Mercedes"},
+      {"name":"E-Class", "type":"SUV", "year": 2023, "dealer_id": 6, "car_make":"Mercedes"},
+      {"name":"A4", "type":"SUV", "year": 2023, "dealer_id": 7, "car_make":"Audi"},
+      {"name":"A5", "type":"SUV", "year": 2023, "dealer_id": 8, "car_make":"Audi"},
+      {"name":"A6", "type":"SUV", "year": 2023, "dealer_id": 9, "car_make":"Audi"},
+      {"name":"Sorrento", "type":"SUV", "year": 2023, "dealer_id": 10, "car_make":"Kia"},
+      {"name":"Carnival", "type":"SUV", "year": 2023, "dealer_id": 11, "car_make":"Kia"},
+      {"name":"Cerato", "type":"Sedan", "year": 2023, "dealer_id": 12, "car_make":"Kia"},
+      {"name":"Corolla", "type":"Sedan", "year": 2023, "dealer_id": 13, "car_make":"Toyota"},
+      {"name":"Camry", "type":"Sedan", "year": 2023, "dealer_id": 14, "car_make":"Toyota"},
+      {"name":"Kluger", "type":"SUV", "year": 2023, "dealer_id": 15, "car_make":"Toyota"},
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+        CarModel.objects.get_or_create(
+            name=data['name'],
+            car_make=car_make_instances[data['car_make']], 
+            defaults={"car_type": data['type'], "year": data['year'], "dealer_id": data['dealer_id']}
+        )
